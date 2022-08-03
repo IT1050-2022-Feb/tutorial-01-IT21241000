65750b1433b561fb175f761c9a0afe99d4ec7f2f
IT21241000 <110513655+IT21241000@users.noreply.github.com>
2022-08-03 04:17:19 +0000

    Tute 01 ans (average of the two marks)

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+	int mark1,mark2;
+	float sum,avg;
+	
+	printf("Enter your mark 1 = ");
+	scanf("%d",&mark1);
+	printf("Enter your mark 2 = ");
+	scanf("%d",&mark2);
+	
+	sum=mark1+mark2;
+	avg=sum/2;
+	
+	printf("average of the two marks =%.2f",avg);
+	return 0;
 }
 
